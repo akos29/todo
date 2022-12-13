139647d0621f62bdea7555a2b8f97456c4e1dbe2
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-13 17:39:14 +0200

    Add Hamburger Menu with coresponding styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
-const Navbar = () => <div>Hello from Navbar</div>;
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <nav className="navBar">
+      <button type="button" onClick={toggleNav}>{navbarOpen ? <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} /> : <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />}</button>
+      <ul className={`menuNav ${navbarOpen ? 'showMenu' : ''}`}>
+        <li><NavLink to="/" activeClassName="active-link" onClick={() => closeMenu()} exact> Home </NavLink></li>
+        <li><NavLink to="about" activeClassName="active-link" onClick={() => closeMenu()} exact> About </NavLink></li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
